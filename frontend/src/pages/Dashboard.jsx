728de8d84661127051d026a8f66710d4776127af
Weverson Godinho <weverson.cleiton22@gmail.com>
2026-06-28 17:59:39 +0100

    Complete the Dashboard page layout and UI design

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,71 +1,133 @@
 //Components
 import Navbar from "../components/Navbar/NavBar.jsx";
-import Footer from "../components/Footer/Footer.jsx"; 
+import Footer from "../components/Footer/Footer.jsx";
+
+//images
+import totalReports from "../assets/totalReports.png";
+import gpsicon from "../assets/gpsicon.png";
+import seat from "../assets/seat.png";
+import busBlue from '../assets/busBlue.png';
 
 function Dashboard() {
   return (
     <>
       <Navbar />
-      <h1>Dashboard</h1>
-        <div className="shadow-sm">
-                <div className="row g-3">
+      <div className="container mt-5">
+        <div className="row align-items-center g-3">
+          <div className="col-12 col-md">
+            <h1 className="mb-0">Dashboard &amp; Analytics</h1>
+          </div>
+
+          <div className="col-12 col-md-auto">
+            <button className="btn btn-success w-100 w-md-auto">
+              Report an Issue
+            </button>
+          </div>
+        </div>
+      </div>
       
-                  <div className="col-12 col-md-6 col-lg-3">
-                    <div className="stat-card">
-                      <div>
-                        <p>Total Reports</p>
-                        <h5>1,455</h5>
-                        <small className="text-success">+10% this month</small>
-                      </div>
-                      <div className="">
-                        <img src="" alt="Green bus icon" />
-                      </div>
-                    </div>
-                  </div>
+      <div className="container mt-5">
+        <div className="row g-4">
 
-                   <div className="col-12 col-md-6 col-lg-3">
-                    <div className="stat-card">
-                      <div>
-                        <p>Total Reports</p>
-                        <h5>1,455</h5> 
-                        <small className="text-success">+10% this month</small>
-                      </div>
-                      <div className="">
-                        <img src="" alt="Green bus icon" />
-                      </div>
-                    </div>
-                  </div>
-
-                   <div className="col-12 col-md-6 col-lg-3">
-                    <div className="stat-card">
-                      <div>
-                        <p>Total Reports</p>
-                        <h5>1,455</h5>       
-                        <small className="text-success">+10% this month</small>
-                      </div>
-                      <div className="">
-                        <img src="" alt="Green bus icon" />
-                      </div>
-                    </div>
-                  </div>
-
-                   <div className="col-12 col-md-6 col-lg-3">
-                    <div className="stat-card">
-                      <div>
-                        <p>Total Reports</p>
-                        <h5>1,455</h5>  
-                        <small className="text-success">+10% this month</small>
-                      </div>
-                      <div className="">
-                        <img src="" alt="Green bus icon" />
-                      </div>
-                    </div>
-                  </div>
-      
-
-      
-                </div>
+          <div className="col-12 col-sm-6 col-xl-3">
+            <div className="stat-card shadow-sm rounded p-4 h-100 d-flex justify-content-between align-items-center">
+              <div>
+                <p className="mb-1 fw-bold">Total Reports</p>
+                <h5>1,455</h5>
+                <small className="text-success">+5% this month</small>
               </div>
+
+              <img
+                src={totalReports}
+                alt="Reports count"
+                width="80"
+                className="img-fluid"
+              />
+            </div>
+          </div>
+
+          <div className="col-12 col-sm-6 col-xl-3">
+            <div className="stat-card shadow-sm rounded p-4 h-100 d-flex justify-content-between align-items-center">
+              <div>
+                <p className="mb-1 fw-bold">Seat Related Issues</p>
+                <h5>65%</h5>
+                <small className="text-success">+10% last month</small>
+              </div>
+
+              <img
+                src={seat}
+                alt="Public transport seat"
+                width="80"
+                className="img-fluid"
+              />
+            </div>
+          </div>
+
+          <div className="col-12 col-sm-6 col-xl-3">
+            <div className="stat-card shadow-sm rounded p-4 h-100 d-flex justify-content-between align-items-center">
+              <div>
+                <p className="mb-1 fw-bold">Most Reported Route</p>
+                <h5>46A</h5>
+                <small className="text-success">100 reports</small>
+              </div>
+
+              <img
+                src={busBlue}
+                alt="Blue bus icon"
+                width="80"
+                className="img-fluid"
+              />
+            </div>
+          </div>
+
+          <div className="col-12 col-sm-6 col-xl-3">
+            <div className="stat-card shadow-sm rounded p-4 h-100 d-flex justify-content-between align-items-center">
+              <div>
+                <p className="mb-1 fw-bold">Counties with Reports</p>
+                <h5>12</h5>
+                <small className="text-success">Of 10 counties</small>
+              </div>
+
+              <img
+                src={gpsicon}
+                alt="GPS icon"
+                width="80"
+                className="img-fluid"
+              />
+            </div>
+          </div>
+
+      </div>
+      </div>
+      <div className="container mt-5">
+        <div className="row g-4">
+
+          <div className="col-12 col-xl-6">
+            <div className="stat-card shadow-sm rounded p-4 h-100 d-flex justify-content-between align-items-center">
+              1   
+            </div>
+          </div>
+          <div className="col-12 col-xl-6">
+            <div className="stat-card shadow-sm rounded p-4 h-100 d-flex justify-content-between align-items-center">
+              1   
+            </div>
+          </div>
+          <div className="col-12 col-xl-6">
+            <div className="stat-card shadow-sm rounded p-4 h-100 d-flex justify-content-between align-items-center">
+              1   
+            </div>
+          </div>
+          <div className="col-12 col-xl-6">
+            <div className="stat-card shadow-sm rounded p-4 h-100 d-flex justify-content-between align-items-center">
+              1   
+            </div>
+          </div>
+          
+
+
+
+        </div>
+      </div>
       <Footer/>
     </>
   )
